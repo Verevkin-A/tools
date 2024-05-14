502a909f7214ff10623b2b92272fc1cfa5f3a576
averevki <sandyverevkin@gmail.com>
2024-05-14 14:05:01 +0200

    Change rhsso from script to kubernetes job

--- base/rhsso/deploy-rhsso.sh
+++ base/rhsso/deploy-rhsso.sh
@@ -4,29 +4,42 @@ set -exuo pipefail
 command -v envsubst
 
 TIMEOUT_TIME="${TIMEOUT_TIME:=125}"
-FILE_ROOT="${BASH_SOURCE%/*}"
+CTL="${CTL:=kubectl}"
+RESOURCES="${BASH_SOURCE%/*}"/resources
 
 NAMESPACE="${NAMESPACE:=tools}"
 ADMIN_USERNAME="${ADMIN_USERNAME:="admin"}"
-ADMIN_PASSWORD="${ADMIN_PASSWORD:="admin"}"
 
-export NAMESPACE ADMIN_PASSWORD ADMIN_USERNAME
+export NAMESPACE ADMIN_USERNAME
 
-function deployRHSSO {
-  <"${FILE_ROOT}"/operator-group.yaml.tpl envsubst | oc apply -n "${NAMESPACE}" -f -
-  oc apply -n "${NAMESPACE}" -f "${FILE_ROOT}"/keycloak-subscription.yaml
-  oc wait -n "${NAMESPACE}" --for=jsonpath=status.installPlanRef.name subscription rhsso-operator --timeout="$TIMEOUT_TIME"s
-  oc wait -n "${NAMESPACE}" --for=condition=Installed installplan --all --timeout="$TIMEOUT_TIME"s
-
-  <"${FILE_ROOT}"/credential-sso-secret.yaml.tpl envsubst | oc apply -n "${NAMESPACE}" -f -
-  oc apply -n "${NAMESPACE}" -f "${FILE_ROOT}"/sso-keycloak.yaml
-  oc apply -n "${NAMESPACE}" -f "${FILE_ROOT}"/no-ssl-sso-service.yaml
-  oc apply -n "${NAMESPACE}" -f "${FILE_ROOT}"/no-ssl-sso-route.yaml
-
-  timeout "$TIMEOUT_TIME" bash -c "oc get statefulset -w -n ${NAMESPACE} -o name | grep -qm1 '^statefulset.apps/keycloak$'"
-  oc rollout -n "${NAMESPACE}" status statefulset/keycloak --timeout="$TIMEOUT_TIME"s
-
-  oc rsh -n "${NAMESPACE}" statefulset/keycloak bash -c "/opt/eap/bin/kcadm.sh update realms/master -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --no-config"
+function set_kubectl_context {
+  $CTL config set-cluster ctx --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+  $CTL config set-credentials user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+  $CTL config set-context ctx --user=user --cluster=ctx
+  $CTL config use-context ctx
 }
 
-deployRHSSO
+function deploy_rhsso {
+  <"${RESOURCES}"/operator-group.yaml.tpl envsubst | $CTL apply -n "${NAMESPACE}" -f -
+  $CTL apply -n "${NAMESPACE}" -f "${RESOURCES}"/keycloak-subscription.yaml
+  $CTL wait -n "${NAMESPACE}" --for=jsonpath=status.installPlanRef.name subscription rhsso-operator --timeout="$TIMEOUT_TIME"s
+  $CTL wait -n "${NAMESPACE}" installplan "$($CTL get -n "${NAMESPACE}" subscription rhsso-operator -o=jsonpath='{.status.installPlanRef.name}')" --for=condition=Installed --timeout="$TIMEOUT_TIME"s
+
+#   <"${RESOURCES}"/credential-sso-secret.yaml.tpl envsubst | $CTL apply -n "${NAMESPACE}" -f -
+  $CTL apply -n "${NAMESPACE}" -f "${RESOURCES}"/sso-keycloak.yaml
+  $CTL apply -n "${NAMESPACE}" -f "${RESOURCES}"/no-ssl-sso-service.yaml
+  $CTL apply -n "${NAMESPACE}" -f "${RESOURCES}"/no-ssl-sso-route.yaml
+
+  timeout "$TIMEOUT_TIME" grep -qm1 '^statefulset.apps/keycloak$' <($CTL get statefulset -w -n "${NAMESPACE}" -o name)
+  $CTL rollout -n "${NAMESPACE}" status statefulset/keycloak --timeout="$TIMEOUT_TIME"s
+
+  ADMIN_PASSWORD="$($CTL get secret credential-sso -o jsonpath='{.data.ADMIN_PASSWORD}' | base64 -d)"
+  $CTL exec --stdin --tty statefulset/keycloak -n "${NAMESPACE}" -- /bin/bash -c "/opt/eap/bin/kcadm.sh update realms/master -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --no-config"
+}
+
+if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]; then
+    NAMESPACE="$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
+    set_kubectl_context  # if running inside kubernetes pod
+fi
+
+deploy_rhsso
